c0f55864c661ec23ba4338ff03c93c42d402d209
ThobileMaku <35804068@student.g.nwu.ac.za>
2023-09-15 13:12:02 +0200

    Dependency injection and added DbContext<SuperStore> to Program.cs

--- SuperStore P3/Program.cs
+++ SuperStore P3/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<SuperStoreContext>(options =>
+    options.UseSqlServer(connectionString));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
